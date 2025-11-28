3dabf3f60cf8a5bc3e05a9e053d2efeee70911e2
tempscailai <temp@scailai.com>
2025-11-28 11:11:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask
 import os
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Python scraper is running"
+    return "Python scraper online"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
